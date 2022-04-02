d6998bc55104a7f0d3cdee952418c1936b8f1bf3
Paolo Chang <pchang@architech.ca>
2022-04-02 00:05:15 -0400

    Leetcode 412 fizzbuzz v2

--- LeetCode/Java/src/com/paolochang/_412_FizzBuzzV2.java
+++ LeetCode/Java/src/com/paolochang/_412_FizzBuzzV2.java
@@ -3,7 +3,7 @@ package LeetCode.Java.src.com.paolochang;
 import java.util.ArrayList;
 import java.util.List;
 
-public class _412_FizzBuzz {
+public class _412_FizzBuzzV2 {
 
   public static void main(String[] args) throws Exception {
     List<String> list = fizzBuzz(15);
@@ -12,16 +12,20 @@ public class _412_FizzBuzz {
 
   public static List<String> fizzBuzz(int n) {
     List<String> list = new ArrayList<String>();
-      
+
       for (int i = 1; i <= n; i++) {
-          if (i % 3 == 0 && i % 5 == 0)
-              list.add("FizzBuzz");
-          else if (i % 3 == 0)
-              list.add("Fizz");
-          else if (i % 5 == 0)
-              list.add("Buzz");
-          else
-              list.add(Integer.toString(i));
+
+        boolean mBy3 = i % 3 == 0;
+        boolean mBy5 = i % 5 == 0;
+
+        if (mBy3)
+          list.add("Fizz");
+        else if (mBy5)
+          list.add("Buzz");
+        else if (mBy3 && mBy5)
+          list.add("FizzBuzz");
+        else
+          list.add(Integer.toString(i));
       }
       
       return list;
