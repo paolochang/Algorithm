68655d17239b0426b381febd811bafc35f563222
Paolo Chang <pchang@architech.ca>
2022-04-07 20:01:53 -0400

    0344 reverse string

--- String/Java/_0344_ReverseString.java
+++ String/Java/_0344_ReverseString.java
@@ -5,17 +5,17 @@ public class _0344_ReverseString {
   public static void main(String[] args) throws Exception {
     char[] s1 = {'h','e','l','l','o'};
     char[] s2 = {'H','a','n','n','a','h'};
-    System.out.println(reverseString(s1));
-    System.out.println(reverseString(s2));
+    reverseString(s1);
+    reverseString(s2);
   }
 
-  public static char[] reverseString(char[] s) {
-    char[] reversed = new char[s.length];
-    for (int i = 0; i < s.length; i++) {
-      reversed[i] = s[s.length-1-i];
+  public static void reverseString(char[] s) {
+    int j = s.length - 1;
+    for (int i = 0; i < s.length/2; i++) {
+      char temp = s[i];
+      s[i] = s[j];
+      s[j] = temp;
+      j--;
     }
-    return reversed;
   }
-
-  public static void reverseStringV2(char[] s) {}
 }
