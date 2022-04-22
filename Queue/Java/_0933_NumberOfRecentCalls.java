554c0e97a1990e5fa39b6ae4ad0d83e31de8d196
Paolo Chang <pchang@architech.ca>
2022-04-21 22:43:52 -0400

    Study note

--- Queue/Java/_0933_NumberOfRecentCalls.java
+++ Queue/Java/_0933_NumberOfRecentCalls.java
@@ -1,5 +1,9 @@
 package Queue.Java;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class _0933_NumberOfRecentCalls {
@@ -24,7 +28,69 @@ public class _0933_NumberOfRecentCalls {
       }
       return count;
     }
-}
+  }
+
+  /**
+   * Queue 
+   * Analysis
+   * Time Complexity: BigO(1)
+   * Space Complexity: BigO(1)
+   */
+  class RecentCounterV2 {
+    Queue<Integer> history;
+    
+    public RecentCounterV2() {
+        this.history = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        this.history.add(t);
+        while(history.element() + 3000 < t)
+            history.remove();
+        
+        return history.size();
+    }
+  }
+  
+  class RecentCounterV3 {
+    List<Integer> history;
+    int oldPing;
+    
+    public RecentCounterV3() {
+        history = new ArrayList<>();
+        oldPing = 0;
+    }
+    
+    public int ping(int t) {
+        history.add(t);
+        
+        if(oldPing < history.size() && history.get(oldPing) < t - 3000)
+            oldPing = search(oldPing);
+        
+        return history.size() - oldPing;
+    }
+    
+    /**
+     * Binary Search
+     * Analysis
+     * Time Complexity: BigO(log n)
+     * Space Complexity: BigO(1)
+     */
+    private int search(int low) {
+        int high = history.size() - 1;
+        int target = history.get(high) - 3000;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(history.get(mid) == target)
+                return mid;
+            else if(history.get(mid) > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+  }
   public static void main(String[] args) throws Exception {
     // ["RecentCounter", "ping", "ping", "ping", "ping"]
     // [[], [1], [100], [3001], [3002]]
