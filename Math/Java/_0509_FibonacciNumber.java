2389075010068668d5ace577507d1f581da26b5e
Paolo Chang <pchang@architech.ca>
2022-04-26 10:53:54 -0400

    Study note

--- Math/Java/_0509_FibonacciNumber.java
+++ Math/Java/_0509_FibonacciNumber.java
@@ -1,7 +1,21 @@
 package Math.Java;
 
 public class _0509_FibonacciNumber {
-  public static int fib(int n) {
+
+  /**
+   * Recursion
+   * CONS: double-exponential time
+   */
+  public static int fibV1(int n) {
+    if (n < 2) return n;
+    return fibV1(n - 1) + fibV1(n - 2);
+  }
+
+  /**
+   * Iteration
+   * CONS: exponential time
+   */
+  public static int fibV2(int n) {
     if (n < 2) return n;
     int a = 0, b = 1, temp;
     for (int i = 1; i < n; i++) {
@@ -12,20 +26,26 @@ public class _0509_FibonacciNumber {
     return b;
   }
 
+  /**
+   * Other options:
+   * Recursive doubling
+   * A^n recursive doubling
+   */
+
   public static void main(String[] args) throws Exception {
     // Input: n = 2
     // Output: 1
     // Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
-    System.out.println(fib(2));
+    System.out.println(fibV1(2));
 
     // Input: n = 3
     // Output: 2
     // Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
-    System.out.println(fib(3));
+    System.out.println(fibV1(3));
 
     // Input: n = 4
     // Output: 3
     // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
-    System.out.println(fib(4));
+    System.out.println(fibV1(4));
   }
 }
--- Tree/Java/_0704_BinarySearch.java
+++ Tree/Java/_0704_BinarySearch.java
@@ -1,7 +1,12 @@
 package Tree.Java;
 
 public class _0704_BinarySearch {
-  public static int search(int[] nums, int target) {
+  /**
+   * Iterative a.k.a Binary Search
+   * Time Complexity: BigO(logN)
+   * Space Complexity: BigO(1)
+   */
+  public static int searchV1(int[] nums, int target) {
     int low = 0;
     int high = nums.length - 1;
     while (low <= high) {
@@ -16,16 +21,39 @@ public class _0704_BinarySearch {
     return -1;
   }
 
+  /**
+   * Recursion
+   * Time Complexity: BigO(logN)
+   * Space Complexity: BigO(logN)
+   */
+  public static int searchV2(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
+    return recursive(nums, low, high, target);
+  }
+
+  public static int recursive(int[] nums, int low, int high, int target) {
+    if(low > high) return -1;
+    int mid = (low + high) / 2;
+    
+    if(nums[mid] == target) return mid;
+    
+    if(target > nums[mid]) return recursive(nums, mid + 1, high, target);
+    else return recursive(nums, low, mid - 1, target);
+  }
+
+
+
   public static void main(String[] args) throws Exception {
     int[] nums1 = {-1,0,3,5,9,12};
-    System.out.println(search(nums1, 9));
-    System.out.println(search(nums1, 2));
+    System.out.println(searchV2(nums1, 9));
+    System.out.println(searchV2(nums1, 2));
 
     int[] nums2 = {5};
-    System.out.println(search(nums2, 5));
-    System.out.println(search(nums2, -5));
+    System.out.println(searchV2(nums2, 5));
+    System.out.println(searchV2(nums2, -5));
 
     int[] nums3 = {2,5};
-    System.out.println(search(nums3, 5));
+    System.out.println(searchV2(nums3, 5));
   }
 }
