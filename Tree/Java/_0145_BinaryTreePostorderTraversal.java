efa89c604807271b27e3d10b26f986b290e8d965
Paolo Chang <pchang@architech.ca>
2022-05-03 21:17:57 -0400

    Add iteration methods

--- Tree/Java/_0145_BinaryTreePostorderTraversal.java
+++ Tree/Java/_0145_BinaryTreePostorderTraversal.java
@@ -2,6 +2,7 @@ package Tree.Java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _0145_BinaryTreePostorderTraversal {
 
@@ -18,7 +19,12 @@ public class _0145_BinaryTreePostorderTraversal {
     }
   }
 
-  public List<Integer> postorderTraversal(TreeNode root) {
+  /**
+   * Recursion
+   * Time Complexity: BigO(n)
+   * Space Complexity: BigO(n)
+   */
+  public List<Integer> postorderTraversalV1(TreeNode root) {
     List<Integer> list = new ArrayList<>();
     postorder(root, list);
     return list;
@@ -33,5 +39,35 @@ public class _0145_BinaryTreePostorderTraversal {
     postorder(root.left, list);
     postorder(root.right, list);
     list.add(root.val);
+  }
+
+  /**
+   * Iteration
+   * Time Complexity: BigO(n)
+   * Space Complexity: BigO(n)
+   */
+  public List<Integer> postorderTraversalV2(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+
+    if (root == null) return list;
+
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+
+    Stack<Integer> out = new Stack<>();
+        
+    while(!stack.isEmpty()) {
+      TreeNode currNode = stack.pop();
+      out.push(currNode.val);
+      
+      if(currNode.left != null)
+        stack.push(currNode.left);
+      if(currNode.right != null)
+        stack.push(currNode.right);
+    }
+    while(!out.isEmpty())
+        list.add(out.pop());
+
+    return list;
   }
 }
