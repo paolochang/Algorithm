43f1a806ddd5bb2285983482f11f4b1bdc2009e6
Paolo Chang <pchang@architech.ca>
2022-06-04 08:15:10 -0400

    0013 Roman to Integer

---
+++ String/Java/_0013_RomantoInteger.java
@@ -0,0 +1,40 @@
+package String.Java;
+
+public class _0013_RomantoInteger {
+  /**
+   * Iteration
+   * Time Complexity: BigO(n)
+   * Space Complexity: BigO(n)
+   */
+  public int romanToInt(String s) {
+    int result = 0, num = 0;
+    for (char str : s.toCharArray()) {
+      switch(str) {
+        case 'I':
+          num = 1;
+          break;
+        case 'V':
+          num = 5;
+          break;
+        case 'X':
+          num = 10;
+          break;
+        case 'L':
+          num = 50;
+          break;
+        case 'C':
+          num = 100;
+          break;
+        case 'D':
+          num = 500;
+          break;
+        case 'M':
+          num = 1000;
+          break;
+      }
+      if (4*num < result) result -= num;
+      else result += num;
+    }
+    return result;
+  }
+}
