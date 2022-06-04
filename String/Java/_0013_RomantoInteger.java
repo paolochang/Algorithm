3e795c272b45349bf151f445b437aa4c9de77962
Paolo Chang <pchang@architech.ca>
2022-06-04 08:18:28 -0400

    0013 Roman to Integer

--- String/Java/_0013_RomantoInteger.java
+++ String/Java/_0013_RomantoInteger.java
@@ -8,8 +8,8 @@ public class _0013_RomantoInteger {
    */
   public int romanToInt(String s) {
     int result = 0, num = 0;
-    for (char str : s.toCharArray()) {
-      switch(str) {
+    for (int i = s.length()-1; i >= 0; i--) {
+      switch(s.charAt(i)) {
         case 'I':
           num = 1;
           break;
