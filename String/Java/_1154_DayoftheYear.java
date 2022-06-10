db263e715f470786c85405840664b990987230b7
Paolo Chang <pchang@architech.ca>
2022-06-10 16:01:50 -0400

    Study note

--- String/Java/_1154_DayoftheYear.java
+++ String/Java/_1154_DayoftheYear.java
@@ -1,5 +1,16 @@
-public class _0000_Template {
+package String.Java;
 
-  public static void main(String[] args) throws Exception {
+public class _1154_DayoftheYear {
+
+  public int dayOfYear(String date) {
+    int[] sumOfDaysPassedOnEachMonth = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+    int day = Integer.parseInt(date.split("-")[2]);
+    int month = Integer.parseInt(date.split("-")[1]);
+    int year = Integer.parseInt(date.split("-")[0]);
+
+    int result = day + sumOfDaysPassedOnEachMonth[month - 1];
+
+    return year % 4 == 0 && month > 2 ? result + 1 : result;
   }
 }
