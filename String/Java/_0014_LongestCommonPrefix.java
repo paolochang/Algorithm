f6d65b2a082c289526b359e5d52844b5916cd68f
Paolo Chang <pchang@architech.ca>
2022-06-30 10:51:24 -0400

    0014 Longest Common Prefix

--- String/Java/_0014_LongestCommonPrefix.java
+++ String/Java/_0014_LongestCommonPrefix.java
@@ -3,15 +3,13 @@ package String.Java;
 public class _0014_LongestCommonPrefix {
 
   public String longestCommonPrefix(String[] strs) {
-    for (String str : strs) {
-        System.out.println(str);
-        
+    String prefix = strs[0];
+    for(int i = 1;i < strs.length; i++){
+        while(strs[i].indexOf(prefix) != 0)
+            prefix = prefix.substring(0, prefix.length() - 1);
+        if(prefix.isEmpty())
+            break;
     }
-    
-    return "test";
-}
-
-public String common(String common, String incoming) {
-        
-}
+    return prefix;
+  }
 }
