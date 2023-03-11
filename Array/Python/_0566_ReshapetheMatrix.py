14b43ca3ac0cb4d222427c5a5479804e5ecc92d8
Paolo Chang <chanhwan.chang@gmail.com>
2023-03-10 20:36:07 -0500

    solved 0566. Reshape the Matrix

---
+++ Array/Python/_0566_ReshapetheMatrix.py
@@ -0,0 +1,54 @@
+from typing import List
+
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if len(mat) * len(mat[0]) != r * c:
+            return mat
+
+        res = []
+        rowlist = []
+
+        for row in range(0, len(mat)):
+            for col in range(0, len(mat[row])):
+                rowlist.append(mat[row][col])
+
+                if len(rowlist) == c:
+                    res.append(rowlist)
+                    rowlist = []
+
+        return res
+
+
+ins = Solution()
+
+print(
+    ins.matrixReshape(
+        [
+            [1, 2],
+            [3, 4],
+        ],
+        1,
+        4,
+    )
+)
+print(
+    ins.matrixReshape(
+        [
+            [1, 2],
+            [3, 4],
+        ],
+        2,
+        4,
+    )
+)
+print(
+    ins.matrixReshape(
+        [
+            [1, 2],
+            [3, 4],
+        ],
+        4,
+        1,
+    )
+)
