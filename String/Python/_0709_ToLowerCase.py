c0c00b8d36c97cf3892f6431ca4bf644e9de034a
Paolo Chang <chanhwan.chang@gmail.com>
2023-03-11 00:47:44 -0500

    solved 0709. To Lower Case

---
+++ String/Python/_0709_ToLowerCase.py
@@ -0,0 +1,17 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        res = ""
+        for c in s:
+            # ord(): find unicode of 'c' (inverse of ord(): chr())
+            if ord("A") <= ord(c) <= ord("Z"):
+                res += chr((ord(c) - ord("A")) + ord("a"))
+            else:
+                res += c
+        return res
+
+
+ins = Solution()
+
+print(ins.toLowerCase("Hello"))
+print(ins.toLowerCase("here"))
+print(ins.toLowerCase("LOVELY"))
---
+++ String/TypeScript/_0709_ToLowerCase.ts
@@ -0,0 +1,12 @@
+function toLowerCase(s: string): string {
+  const str = s.split("");
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].charCodeAt(0) > 64 && str[i].charCodeAt(0) < 91)
+      str[i] = String.fromCharCode(str[i].charCodeAt(0) + 32);
+  }
+  return str.join("");
+}
+
+console.log(toLowerCase("Hello"));
+console.log(toLowerCase("here"));
+console.log(toLowerCase("LOVELY"));
